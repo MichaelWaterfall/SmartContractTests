d492f2761647aebddd6e7c330e1891dca49f3578
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2023-11-26 11:15:37 +0000

    TestCryptoKeeperFactory.sol added

---
+++ contracts/TestCryptoKeeperFactory.sol
@@ -0,0 +1,60 @@
+//SDPX-License-Identifier: Unlicensed
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/proxy/Clones.sol";
+import "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./ICryptoKeeper.sol";
+
+contract TestCryptoKeeperFactory is Ownable
+{
+    using Clones for address;
+
+    event CryptoKeeperCreated(address _cryptoKeeperAddress);
+    event TemplateChanged(address _previousAddress, address _newAddress);
+
+    address public templateAddress;
+
+    constructor(address _owner, address _templateAddress) {
+        require(_templateAddress != address(0));
+        setTemplateAddress(_templateAddress);
+        _transferOwnership(_owner);
+    }
+
+    function setTemplateAddress(address _templateAddress) public onlyOwner {
+        require(
+            ERC165Checker.supportsInterface(
+                _templateAddress,
+                type(ICryptoKeeper).interfaceId
+            ),
+            "Interface not supported"
+        );
+        address previousAddress = templateAddress;
+        templateAddress = _templateAddress;
+        emit TemplateChanged(previousAddress, _templateAddress);
+    }
+
+    function createCryptoKeeper(
+        bytes32 _salt,
+        address[] calldata _operators
+    )
+        external
+        returns (address)
+    {
+        address cryptoKeeperAddress = templateAddress.cloneDeterministic(_salt);
+
+        ICryptoKeeper(cryptoKeeperAddress).initialize(_operators);
+
+        emit CryptoKeeperCreated(cryptoKeeperAddress);
+        return cryptoKeeperAddress;
+    }
+
+    function predictCryptoKeeperAddress(bytes32 _salt)
+        public
+        view
+        returns (address)
+    {
+        return
+            templateAddress.predictDeterministicAddress(_salt, address(this));
+    }
+}
