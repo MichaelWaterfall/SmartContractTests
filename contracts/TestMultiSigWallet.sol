73a0c636e072df4ac161038e66104b1d70811ec6
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2023-11-28 10:52:30 +0000

    TestMultiSigWallet.sol added

---
+++ contracts/TestMultiSigWallet.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.13;
+
+contract TestMultiSigSafe { 
+
+    address[] public signers;
+    uint8 public requiredApprovals;
+
+    // To -> Amount -> Approvals
+    mapping(address => mapping(uint256 => uint256)) public withdrawRequests;
+
+    constructor(address[] memory _signers, uint8 _requiredApprovals) {
+
+        requiredApprovals = _requiredApprovals;
+        for(uint i = 0; i < _signers.length; i++) {
+            signers.push(_signers[i]);
+        }
+    }
+    
+    function withdrawETH(address _to, uint _amount) external {
+
+        uint approvals = withdrawRequests[_to][_amount];
+        require(approvals >= requiredApprovals, "Not enough approvals");
+
+        withdrawRequests[_to][_amount] = 0;
+        payable(_to).send(_amount);
+    }
+}
