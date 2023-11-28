663b4fa7e953697e04a113fd8f4ec27ed338a3a5
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2023-11-28 08:24:24 +0000

    testFlash.js added

---
+++ test/testFlash.js
@@ -0,0 +1,13 @@
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
+
+describe('Test Smart Contract', function () {
+
+    let deployer, user;
+    const POOL_BALANCE = ethers.utils.parseEther('1000');
+
+    it('Test Exploit', async function () {
+        
+    });
+
+});
