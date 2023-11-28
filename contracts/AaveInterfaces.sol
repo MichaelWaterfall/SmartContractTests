e6d99f2c28863cc80f831620d9984f06a971299f
Michael Waterfall <97835853+MichaelWaterfall@users.noreply.github.com>
2023-11-28 11:38:29 +0000

    AaveInterfaces.sol added

---
+++ contracts/AaveInterfaces.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.13;
+interface IPool {
+    function supply(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
+    function withdraw(address asset, uint256 amount, address to) external returns (uint256);
+    function borrow(address asset, uint256 amount, uint256 interestRateMode, uint16 referralCode, address onBehalfOf) external;
+    function repay(address asset, uint256 amount, uint256 rateMode, address onBehalfOf) external returns (uint256);
+}
